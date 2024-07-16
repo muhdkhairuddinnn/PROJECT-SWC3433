9eb92462dff0f3e1fcbac74dda1600384b1c6342
muhdkhairuddinnn <KL2311015305@student.uptm.edu.my>
2024-07-16 22:23:31 +0800

    ONLINE DELIVERY FOOD WEBSITE

---
+++ food/restaurants.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");
+error_reporting(0);
+session_start();
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="images/logo.jpg">
+    <title>Restaurants</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet"> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <style>
+        /* Optional: Add some styling to active link */
+        .nav-item.active .nav-link {
+            font-weight: bold;
+            color: red; /* Change color as needed */
+        }
+    </style>
+</head>
+
+<body>
+
+        <header id="header" class="header-scroll top-header headrom">
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/icn.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="#bottom">About Us <span class="sr-only"></span></a> </li>
+                            
+							<?php
+						if(empty($_SESSION["user_id"]))
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+							}
+						else
+							{
+									
+									
+										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="page-wrapper">
+            <div class="top-links">
+                <div class="container">
+                    <ul class="row links">
+                       
+                        <li class="col-xs-12 col-sm-4 link-item active"><span>1</span><a href="#">Choose Restaurant</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item"><span>2</span><a href="#">Pick Your favorite food</a></li>
+                        <li class="col-xs-12 col-sm-4 link-item"><span>3</span><a href="#">Order and Pay</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="inner-page-hero bg-image" data-image-src="images/img/pimg.jpg">
+                <div class="container"> </div>
+            </div>
+            <div class="result-show">
+                <div class="container">
+                    <div class="row">     
+                    </div>
+                </div>
+            </div>
+            <section class="restaurants-page">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-5 col-md-5 col-lg-3">
+                        </div>
+                        <div class="col-xs-12 col-sm-7 col-md-7 col-lg-9">
+                            <div class="bg-gray restaurant-entry">
+                                <div class="row">
+								<?php $ress= mysqli_query($db,"select * from restaurant");
+									      while($rows=mysqli_fetch_array($ress))
+										  {
+													
+						
+													 echo' <div class="col-sm-12 col-md-12 col-lg-8 text-xs-center text-sm-left">
+															<div class="entry-logo">
+																<a class="img-fluid" href="dishes.php?res_id='.$rows['rs_id'].'" > <img src="admin/Res_img/'.$rows['image'].'" alt="Food logo"></a>
+															</div>
+															<!-- end:Logo -->
+															<div class="entry-dscr">
+																<h5><a href="dishes.php?res_id='.$rows['rs_id'].'" >'.$rows['title'].'</a></h5> <span>'.$rows['address'].'</span>
+																
+															</div>
+															<!-- end:Entry description -->
+														</div>
+														
+														 <div class="col-sm-12 col-md-12 col-lg-4 text-xs-center">
+																<div class="right-content bg-white">
+																	<div class="right-review">
+																		
+																		<a href="dishes.php?res_id='.$rows['rs_id'].'" class="btn btn-purple">View Menu</a> </div>
+																</div>
+																<!-- end:right info -->
+															</div>';
+										  }
+						
+						
+						?>
+                                    
+                                </div>
+                
+                            </div>
+                         
+                            
+                                
+                            </div>
+                          
+                          
+                           
+                        </div>
+                    </div>
+                </div>
+            </section>
+       
+        <footer class="footer">
+            <div class="container">
+                
+              
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                            <h5>Payment Options</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                                    <h5>Address</h5>
+                                    <p>30 Lorong Bahagia, Mersing Kecil</p>
+                                   <h5><a href="https://wa.me/0108051072" target="_blank">Phone: 0108051072</a></h5>
+                                    <h5><a href="https://mail.google.com/mail/?view=cm&fs=1&to=youngbroke0@gmail.com" target="_blank">Email: youngbroke0@gmail.com</a></h5>
+                                    </div></h5>
+                                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                                    <h5>About Us</h5>
+                                   <p>Welcome to OFOS! We specialize in bringing you the finest dishes from top-rated 5-star restaurants, ensuring every meal is a delightful experience. Discover quality food and convenience at your fingertips with OFOS.<br></p>
+                                   <h5>Mission</h5>
+                                   <p>Our mission at OFOS is to redefine convenience by offering a seamless online food ordering experience that connects food lovers with top-rated restaurants, ensuring every meal is a memorable one.</p>
+                                </div>
+                    </div>
+                </div>
+       
+            </div>
+        </footer>
+        
+     <div id="bottom">
+        <!-- Content at the bottom of the webpage -->
+    </div>   
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $('a[href^="#"]').on('click', function(event) {
+            var target = $(this.getAttribute('href'));
+            if( target.length ) {
+                event.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+            }
+        });
+    });
+</script>
+
+</body>
+
+</html>
