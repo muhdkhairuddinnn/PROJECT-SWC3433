8f572efcdf23b7ad397f47faf21e9a9780f62e0e
muhdkhairuddinnn <KL2311015305@student.uptm.edu.my>
2024-07-16 23:38:57 +0800

    ONLINE FOOD DELIVERY WEBSITE

---
+++ food/registration.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+session_start(); 
+error_reporting(0); 
+include("connection/connect.php"); 
+if(isset($_POST['submit'] )) 
+{
+     if(empty($_POST['firstname']) || 
+   	    empty($_POST['lastname'])|| 
+		empty($_POST['email']) ||  
+		empty($_POST['phone'])||
+		empty($_POST['password'])||
+		empty($_POST['cpassword']) ||
+		empty($_POST['cpassword']))
+		{
+			$message = "All fields must be Required!";
+		}
+	else
+	{
+	
+	$check_username= mysqli_query($db, "SELECT username FROM users where username = '".$_POST['username']."' ");
+	$check_email = mysqli_query($db, "SELECT email FROM users where email = '".$_POST['email']."' ");
+		
+
+	
+	if($_POST['password'] != $_POST['cpassword']){  
+       	
+          echo "<script>alert('Password not match');</script>"; 
+    }
+	elseif(strlen($_POST['password']) < 6)  
+	{
+      echo "<script>alert('Password Must be >=6');</script>"; 
+	}
+	elseif(strlen($_POST['phone']) < 10)  
+	{
+      echo "<script>alert('Invalid phone number!');</script>"; 
+	}
+
+    elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) 
+    {
+          echo "<script>alert('Invalid email address please type a valid email!');</script>"; 
+    }
+	elseif(mysqli_num_rows($check_username) > 0) 
+     {
+       echo "<script>alert('Username Already exists!');</script>"; 
+     }
+	elseif(mysqli_num_rows($check_email) > 0) 
+     {
+       echo "<script>alert('Email Already exists!');</script>"; 
+     }
+	else{
+       
+	 
+	$mql = "INSERT INTO users(username,f_name,l_name,email,phone,password,address) VALUES('".$_POST['username']."','".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['phone']."','".md5($_POST['password'])."','".$_POST['address']."')";
+	mysqli_query($db, $mql);
+	
+		 header("refresh:0.1;url=login.php");
+    }
+	}
+
+}
+
+
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="images/logo.jpg">
+    <title>Registration</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <style>
+        /* Optional: Add some styling to active link */
+        .nav-item.active .nav-link {
+            font-weight: bold;
+            color: red; /* Change color as needed */
+        }
+    </style>
+</head>
+<body>
+<div style=" background-image: url('images/img/pimg.jpg');">
+         <header id="header" class="header-scroll top-header headrom">
+            <nav class="navbar navbar-dark">
+               <div class="container">
+                  <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                  <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/icn.png" alt=""> </a>
+                  <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                     <ul class="nav navbar-nav">
+							<li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="#bottom">About Us <span class="sr-only"></span></a> </li>
+                            
+							<?php
+						if(empty($_SESSION["user_id"]))
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+							}
+						else
+							{
+									
+									
+										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+                  </div>
+               </div>
+            </nav>
+         </header>
+         <div class="page-wrapper">
+            
+               <div class="container">
+                  <ul>
+                    
+                    
+                  </ul>
+               </div>
+            
+            <section class="contact-page inner-page">
+               <div class="container ">
+                  <div class="row ">
+                     <div class="col-md-12">
+                        <div class="widget" >
+                           <div class="widget-body">
+                            
+							  <form action="" method="post">
+                                 <div class="row">
+								  <div class="form-group col-sm-12">
+                                       <label for="exampleInputEmail1">User-Name</label>
+                                       <input class="form-control" type="text" name="username" id="example-text-input"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputEmail1">First Name</label>
+                                       <input class="form-control" type="text" name="firstname" id="example-text-input"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputEmail1">Last Name</label>
+                                       <input class="form-control" type="text" name="lastname" id="example-text-input-2"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputEmail1">Email Address</label>
+                                       <input type="text" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputEmail1">Phone number</label>
+                                       <input class="form-control" type="text" name="phone" id="example-tel-input-3"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputPassword1">Password</label>
+                                       <input type="password" class="form-control" name="password" id="exampleInputPassword1"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputPassword1">Confirm password</label>
+                                       <input type="password" class="form-control" name="cpassword" id="exampleInputPassword2"> 
+                                    </div>
+									 <div class="form-group col-sm-12">
+                                       <label for="exampleTextarea">Delivery Address</label>
+                                       <textarea class="form-control" id="exampleTextarea"  name="address" rows="3"></textarea>
+                                    </div>
+                                   
+                                 </div>
+                                
+                                 <div class="row">
+                                    <div class="col-sm-4">
+                                       <p> <input type="submit" value="Register" name="submit" class="btn theme-btn"> </p>
+                                    </div>
+                                 </div>
+                              </form>
+                  
+						   </div>
+           
+                        </div>
+                     
+                     </div>
+                    
+                  </div>
+               </div>
+            </section>
+            
+      
+            <footer class="footer">
+               <div class="container">
+           
+                  <div class="row bottom-footer">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                              <h5>Payment Options</h5>
+                              <ul>
+                                 <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                 </li>
+                                 <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                 </li>
+                                 <li>
+                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                                 </li>
+                                 <li>
+                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                                 </li>
+                                 <li>
+                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                                 </li>
+                              </ul>
+                           </div>
+                           <div class="col-xs-12 col-sm-4 address color-gray">
+                                    <h5>Address</h5>
+                                    <p>30 Lorong Bahagia, Mersing Kecil</p>
+                                    <h5><a href="https://wa.me/0108051072" target="_blank">Phone: 0108051072</a></h5>
+                                    <h5><a href="https://mail.google.com/mail/?view=cm&fs=1&to=youngbroke0@gmail.com" target="_blank">Email: youngbroke0@gmail.com</a></h5>
+                                    </div></h5>
+                                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                                    <h5>About Us</h5>
+                                   <p>Welcome to OFOS! We specialize in bringing you the finest dishes from top-rated 5-star restaurants, ensuring every meal is a delightful experience. Discover quality food and convenience at your fingertips with OFOS.<br></p>
+                                   <h5>Mission</h5>
+                                   <p>Our mission at OFOS is to redefine convenience by offering a seamless online food ordering experience that connects food lovers with top-rated restaurants, ensuring every meal is a memorable one.</p>
+                                </div>
+                        </div>
+                     </div>
+                  </div>
+      
+               </div>
+            </footer>
+         
+         </div>
+       
+   <div id="bottom">
+        <!-- Content at the bottom of the webpage -->
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $('a[href^="#"]').on('click', function(event) {
+            var target = $(this.getAttribute('href'));
+            if( target.length ) {
+                event.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+            }
+        });
+    });
+</script>
+
+</body>
+
+</html>
